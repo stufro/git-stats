13d5931e4724e228294c6e5d130b213cedef65f3
Stuart Frost <stuart.2.frost@bt.com>
2022-02-23 19:26:49 +0000

    setup webpack and .env

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,42 @@
 module Main exposing(..)
 
+import Browser exposing (element)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
-main = span [class "welcome"] [text "Hello World"]
+main : Program String Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+ 
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+init : String -> ( Model, Cmd Msg )
+init pw =
+    ( { initialModel | githubPass = pw }, Cmd.none )
+
+type alias Model =
+  { githubPass :  String }
+
+initialModel : Model
+initialModel =
+  { githubPass = "" }
+
+type Msg =
+  NoOp
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+      NoOp -> ( model, Cmd.none )
+
+view : Model -> Html Msg
+view model =
+  text "Hello, World!"
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import { Elm } from './Main.elm'
 
-var app = Elm.Main.init({node: document.getElementById('elm-app')});
+var app = Elm.Main.init({
+  node: document.getElementById('elm-app'),
+  flags: process.env.GITHUB_PASSWORD
+});
