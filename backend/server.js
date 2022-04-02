fcff349d6d25c72d18f755966444e0ffbd68eebf
Stuart Frost <stuart.2.frost@bt.com>
2022-04-02 20:03:46 +0100

    move backend code into serverless module to use on netlify

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+'use strict';
+
+import fetch from "node-fetch"
+import dotenv from "dotenv"
+import express from "express"
+import serverless from "serverless-http"
+import bodyParser from "body-parser"
+const app = express();
+const router = express.Router();
+
+dotenv.config();
+
+router.get('/user/:name', async (req, res) => {
+  var response = await fetch(`http://api.github.com/users/${req.params.name}`);
+  var data = await response.json();
+  console.log(response.headers);
+  res.status(response.status).send(data);
+})
+
+router.get('/user/:name/repos', async (req, res) => {
+  var response = await fetch(`http://api.github.com/users/${req.params.name}/repos?per_page=100`);
+  var data = await response.json();
+  res.status(response.status).send(data);
+})
+
+router.get('/user/:name/last_activity', async (req, res) => {
+  var response = await fetch(`http://api.github.com/users/${req.params.name}/events?per_page=1`);
+  var data = await response.json();
+  res.status(response.status).send(data);
+})
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);
+
+export default app;
+const handler = serverless(app); 
+export { handler };
--- local-server.js
+++ local-server.js
@@ -1,28 +1,6 @@
-import fetch from "node-fetch";
-import dotenv from "dotenv";
-import express from "express"
-dotenv.config();
-const app = express();
-const port = 3000;
+'use strict';
 
-app.get('/user/:name', async (req, res) => {
-  var response = await fetch(`http://api.github.com/users/${req.params.name}`);
-  var data = await response.json();
-  res.status(response.status).send(data);
-})
+import app from './backend/server.js'
 
-app.get('/user/:name/repos', async (req, res) => {
-  var response = await fetch(`http://api.github.com/users/${req.params.name}/repos?per_page=100`);
-  var data = await response.json();
-  res.status(response.status).send(data);
-})
+app.listen(3000, () => console.log('Local app listening on port 3000!'));
 
-app.get('/user/:name/last_activity', async (req, res) => {
-  var response = await fetch(`http://api.github.com/users/${req.params.name}/events?per_page=1`);
-  var data = await response.json();
-  res.status(response.status).send(data);
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-})
