3ab9ee4ad92b5b0ff48bc795a8768a6cdcc4af02
Stuart Frost <stuart.2.frost@bt.com>
2022-04-16 20:53:05 +0100

    fetch multiple pages of repo data

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,23 @@ router.get('/user/:name', async (req, res) => {
 
 router.get('/user/:name/repos', async (req, res) => {
   var githubResponse = await githubRequest(`${req.params.name}/repos?per_page=100`, res);
-  res.status(githubResponse.status).send(githubResponse.data);
+  var repos = githubResponse.data
+
+  var repos = await fetchSubsequentPages(repos, req.params.name, githubResponse.link, res)
+
+  res.status(githubResponse.status).send(repos);
 })
+
+async function fetchSubsequentPages(results, username, link, res) {
+  if (typeof link === "string" && (link.match(/<(\S+)>; rel="next"/) !== null)) {
+    var newParams = link.match(/<(\S+)\>; rel="next"/)[1].split("?")[1]
+    var githubResponse = await githubRequest(`${username}/repos?${newParams}`, res);
+    var concatResults = results.concat(githubResponse.data)
+    return await fetchSubsequentPages(concatResults, username, githubResponse.link, res)
+  } else {
+    return results
+  }
+}
 
 router.get('/user/:name/last_activity', async (req, res) => {
   var githubResponse = await githubRequest(`${req.params.name}/events?per_page=1`, res);
@@ -30,7 +45,7 @@ async function githubRequest(endpoint, res) {
   });
   var data = await response.json();
   res.set('Access-Control-Allow-Origin', '*');
-  return new Object({status: response.status, data: data})
+  return new Object({status: response.status, data: data, link: response.headers.get("link")})
 }
 
 app.use(bodyParser.json());
